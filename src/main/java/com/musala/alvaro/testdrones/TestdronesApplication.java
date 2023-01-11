d41b1dec34ddbc1e4d4d4a7667970205234909d4
Alvaro Quiles Lopez <aquileslopez8701@gmail.com>
2023-01-11 13:19:18 -0500

    initial commit

---
+++ src/main/java/com/musala/alvaro/testdrones/TestdronesApplication.java
@@ -0,0 +1,13 @@
+package com.musala.alvaro.testdrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdronesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/alvaro/testdrones/TestdronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.alvaro.testdrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
