34946e845eeab2dc0567ca81f92ea13d27a95829
Alvaro Quiles Lopez <aquileslopez8701@gmail.com>
2023-01-31 23:34:47 -0500

    security ok

--- src/main/java/com/musala/alvaro/testdrones/configuration/security/JwtUtils.java
+++ src/main/java/com/musala/alvaro/testdrones/configuration/security/JwtUtils.java
@@ -9,9 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -69,7 +67,6 @@ public class JwtUtils {
     try {
     	SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret)); 
     	Jwts.builder().signWith(secretKey, SignatureAlgorithm.HS512).claim("Bearer", authToken);
-    	//Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(authToken);
       return true;
     } catch (MalformedJwtException e) {
       logger.error("Invalid JWT token: {}", e.getMessage());
